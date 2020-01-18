fcb3c5bfca584d8fb5749f4e4c6b3e18ea04ee67
Antoine <antoine_moghaddar@hotmail.com>
2020-01-18 18:29:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/faker/Main/body.java
@@ -0,0 +1,16 @@
+package com.example.faker.Main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.faker.R;
+
+public class body extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_body);
+    }
+}
